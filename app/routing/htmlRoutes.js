d3682514af21748fcab85370c6dbbd439bf6f55e
mczuckermann <matt.zuckermann@my.wheaton.edu>
2018-12-30 18:21:57 -0800

    htmlRoutes.js file completed for routing between HTML in localhost

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,23 @@
-const PATH = require("path");
 const express = require("express");
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const connection = app.connect();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// app.use("/static", express.static(path.join(__dirname, "assets")));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+app.listen(PORT, function () {
+    console.log(__dirname);
+    console.log("App listening on PORT http://localhost:" + PORT);
+});
