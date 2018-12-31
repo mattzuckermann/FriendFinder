ea57c6001eec51e53af1f69f1655358ac7933738
mczuckermann <matt.zuckermann@my.wheaton.edu>
2018-12-31 11:36:07 -0800

    Updated file structure to link inputs of forms to server

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,16 @@
 const friendsData = require("../data/friends");
 
+$("#surveySubmit").on("click", function () {
+    function checkValid() {
+        let formFilled = true;
+        if ($(".form-control").each().val() === "" || $(".form-control").each().val() === null || $(".custom-select").each().children().val() === "Select an Option") {
+            let formFilled = false;
+        }
+        return formFilled;
+    }
+    console.log(checkValid());
+});
+
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
