62973268faded60221131051b38181743d4818cb
mczuckermann <matt.zuckermann@my.wheaton.edu>
2018-12-31 11:37:42 -0800

    commented out jquery code to deploy properly to heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,15 @@
 const friendsData = require("../data/friends");
 
-$("#surveySubmit").on("click", function () {
-    function checkValid() {
-        let formFilled = true;
-        if ($(".form-control").each().val() === "" || $(".form-control").each().val() === null || $(".custom-select").each().children().val() === "Select an Option") {
-            let formFilled = false;
-        }
-        return formFilled;
-    }
-    console.log(checkValid());
-});
+// $("#surveySubmit").on("click", function () {
+//     function checkValid() {
+//         let formFilled = true;
+//         if ($(".form-control").each().val() === "" || $(".form-control").each().val() === null || $(".custom-select").each().children().val() === "Select an Option") {
+//             let formFilled = false;
+//         }
+//         return formFilled;
+//     }
+//     console.log(checkValid());
+// });
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
