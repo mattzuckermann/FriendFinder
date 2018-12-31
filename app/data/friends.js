1867323694054ebd3e432e22f7a19bf1b13bcf29
mczuckermann <matt.zuckermann@my.wheaton.edu>
2018-12-30 20:44:03 -0800

    Put in two placeholder friends objects for testing

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+const friendsData = [
+    {
+        name: "Ahmed",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQptnSkzbgCFeEiDwTv3TPt9BWJ45Fd7qpo99ro41mlIbVz8Ezn",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Elizabeth",
+        photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        scores: [
+            3,
+            2,
+            1,
+            6,
+            3,
+            2,
+            5,
+            2,
+            4,
+            2
+        ]
+    }
+];
+
+module.exports = friendsData;
