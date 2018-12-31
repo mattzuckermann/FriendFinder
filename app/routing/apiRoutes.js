639eaf50cb087177c24c70e079a3a8f2596aff52
mczuckermann <matt.zuckermann@my.wheaton.edu>
2018-12-30 20:42:05 -0800

    Updated routing between api & html making server exclusive using imports

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,16 @@
-const PATH = require("path");
+const friendsData = require("../data/friends");
 const express = require("express");
-
 const app = express();
 
-const connection = app.connect();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+    app.post("/api/friends", function (req, res) {
+        friendsData.push(req.body);
+        res.json(true);
+    });
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,23 +1,20 @@
-const express = require("express");
 const path = require("path");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
 
-// app.use("/static", express.static(path.join(__dirname, "assets")));
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
-
-app.listen(PORT, function () {
-    console.log(__dirname);
-    console.log("App listening on PORT http://localhost:" + PORT);
-});
+    app.listen(PORT, function () {
+        console.log(__dirname);
+        console.log("App listening on PORT http://localhost:" + PORT);
+    });
+}
