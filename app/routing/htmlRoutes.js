cfb771cf54ac4833bde7eaebcd90fc0bd3230976
mczuckermann <matt.zuckermann@my.wheaton.edu>
2019-01-08 03:32:18 -0600

    Created redirect path for anything other than paths given

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,6 +10,6 @@ module.exports = function (app) {
     });
 
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.redirect('/');
     });
 };
