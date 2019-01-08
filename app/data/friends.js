3e0585f5004c8770aaee58db5064bc657ae1e4fc
mczuckermann <matt.zuckermann@my.wheaton.edu>
2019-01-08 03:54:21 -0600

    Surrounded numbers in score list with quotes

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,48 +3,48 @@ const friendsData = [
         name: "Ahmed",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQptnSkzbgCFeEiDwTv3TPt9BWJ45Fd7qpo99ro41mlIbVz8Ezn",
         scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
         ]
     },
     {
         name: "Adam",
         photo: "https://celebslikeme.blob.core.windows.net/resources/scroll003.jpg",
         scores: [
-            3,
-            2,
-            1,
-            5,
-            2,
-            3,
-            5,
-            1,
-            2,
-            4
+            "3",
+            "2",
+            "1",
+            "5",
+            "2",
+            "3",
+            "5",
+            "1",
+            "2",
+            "4"
         ]
     },
     {
         name: "Elizabeth",
         photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
         scores: [
-            3,
-            2,
-            1,
-            6,
-            3,
-            2,
-            5,
-            2,
-            4,
-            2
+            "3",
+            "2",
+            "1",
+            "6",
+            "3",
+            "2",
+            "5",
+            "2",
+            "4",
+            "2"
         ]
     }
 ];
